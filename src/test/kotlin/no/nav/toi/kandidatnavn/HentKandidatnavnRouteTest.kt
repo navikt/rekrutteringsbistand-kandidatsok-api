a3ac227c8c5ea65164c849de2f446733cc727365
Are Husby <are.husby@nav.no>
2024-02-21 15:45:39 +0100

    Fiks feil spm følge av å merge merge inn main-branch

--- src/test/kotlin/no/nav/toi/kandidatnavn/HentKandidatnavnRouteTest.kt
+++ src/test/kotlin/no/nav/toi/kandidatnavn/HentKandidatnavnRouteTest.kt
@@ -10,36 +10,26 @@ import com.github.kittinunf.result.Result
 import com.github.tomakehurst.wiremock.client.WireMock
 import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo
 import com.github.tomakehurst.wiremock.junit5.WireMockTest
-import com.nimbusds.jwt.SignedJWT
-import no.nav.security.mock.oauth2.MockOAuth2Server
-import no.nav.toi.App
-import no.nav.toi.RolleUuidSpesifikasjon
+import no.nav.toi.LokalApp
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
-import java.util.*
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @WireMockTest(httpPort = 10000)
 class HentKandidatnavnRouteTest {
-    private val modiaGenerell = "67a06857-0028-4a90-bf4c-9c9a92c7d733"
-    private val modiaOppfølging = "554a66fb-fbec-4b92-90c1-0d9c085c362c"
-    private val authPort = 18306
-    private val app: App = lagLokalApp()
-    private val authServer = MockOAuth2Server()
+    private val app = LokalApp()
 
     @BeforeAll
     fun setUp() {
         app.start()
-        authServer.start(port = authPort)
     }
 
     @AfterAll
     fun tearDown() {
         app.close()
-        authServer.shutdown()
     }
 
     @Test
@@ -126,37 +116,10 @@ class HentKandidatnavnRouteTest {
         Assertions.assertThat(responseFeilValue.statusCode).isEqualTo(400)
     }
 
-
-    private fun lagLokalApp() = App(
-        port = 8080,
-        azureAppClientId = "1",
-        azureOpenidConfigIssuer = "http://localhost:$authPort/default",
-        azureOpenidConfigJwksUri = "http://localhost:$authPort/default/jwks",
-        rolleUuidSpesifikasjon = RolleUuidSpesifikasjon(
-            modiaGenerell = UUID.fromString(modiaGenerell),
-            modiaOppfølging = UUID.fromString(modiaOppfølging),
-        ),
-        openSearchUsername = "user",
-        openSearchPassword = "pass",
-        openSearchUri = "http://localhost:10000/opensearch",
-    )
-
-    private fun token(
-        issuerId: String = "http://localhost:$authPort/default",
-        aud: String = "1",
-        navIdent: String = "A000001",
-        claims: Map<String, Any> = mapOf("NAVident" to navIdent, "groups" to listOf(modiaGenerell))
-    ): SignedJWT = authServer.issueToken(
-        issuerId = issuerId,
-        subject = "subject",
-        audience = aud,
-        claims = claims
-    )
-
     private fun sendRequest(body: String): Triple<Request, Response, Result<String, FuelError>> =
         Fuel.post("http://localhost:8080/api/hent-kandidatnavn")
             .jsonBody(body)
-            .header("Authorization", "Bearer ${token().serialize()}")
+            .header("Authorization", "Bearer ${app.lagToken().serialize()}")
             .responseString()
 
 
