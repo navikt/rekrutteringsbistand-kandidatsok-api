4560bdeaa62881ca476b3aead946ce8c2712dd4f
frodank <11638682+frodank@users.noreply.github.com>
2024-06-04 14:31:20 +0200

    Ctrl+c -> Ctrl+v-feil

--- src/main/kotlin/no/nav/toi/Main.kt
+++ src/main/kotlin/no/nav/toi/Main.kt
@@ -140,9 +140,9 @@ fun main() {
         ),
         rolleUuidSpesifikasjon = RolleUuidSpesifikasjon(
             modiaGenerell = UUID.fromString(getenv("MODIA_GENERELL_GRUPPE")),
-            jobbsøkerrettet = UUID.fromString(getenv("MODIA_GENERELL_GRUPPE")),
-            arbeidsgiverrettet = UUID.fromString(getenv("MODIA_GENERELL_GRUPPE")),
-            utvikler = UUID.fromString(getenv("MODIA_GENERELL_GRUPPE"))
+            jobbsøkerrettet = UUID.fromString(getenv("REKRUTTERINGSBISTAND_JOBBSOKERRETTET")),
+            arbeidsgiverrettet = UUID.fromString(getenv("REKRUTTERINGSBISTAND_ARBEIDSGIVERRETTET")),
+            utvikler = UUID.fromString(getenv("REKRUTTERINGSBISTAND_UTVIKLER"))
         ),
         openSearchUsername = getenv("OPEN_SEARCH_USERNAME"),
         openSearchPassword = getenv("OPEN_SEARCH_PASSWORD"),
